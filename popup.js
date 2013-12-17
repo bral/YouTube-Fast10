6f1ea547d40072f05173f9a4c21b6ff8c4fa286e
Brannon Lucas <brannonlucas@gmail.com>
2013-12-16 19:36:31 -0800

    search box functions, brings up new search results in popup, links working.

--- popup.js
+++ popup.js
@@ -1,41 +1,41 @@
-// Changes XML to JSON
-function xmlToJson(xml) {
+// // Changes XML to JSON
+// function xmlToJson(xml) {
 
-  // Create the return object
-  var obj = {};
+//   // Create the return object
+//   var obj = {};
 
-  if (xml.nodeType == 1) { // element
-    // do attributes
-    if (xml.attributes.length > 0) {
-    obj["@attributes"] = {};
-      for (var j = 0; j < xml.attributes.length; j++) {
-        var attribute = xml.attributes.item(j);
-        obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-      }
-    }
-  } else if (xml.nodeType == 3) { // text
-    obj = xml.nodeValue;
-  }
+//   if (xml.nodeType == 1) { // element
+//     // do attributes
+//     if (xml.attributes.length > 0) {
+//     obj["@attributes"] = {};
+//       for (var j = 0; j < xml.attributes.length; j++) {
+//         var attribute = xml.attributes.item(j);
+//         obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+//       }
+//     }
+//   } else if (xml.nodeType == 3) { // text
+//     obj = xml.nodeValue;
+//   }
 
-  // do children
-  if (xml.hasChildNodes()) {
-    for(var i = 0; i < xml.childNodes.length; i++) {
-      var item = xml.childNodes.item(i);
-      var nodeName = item.nodeName;
-      if (typeof(obj[nodeName]) == "undefined") {
-        obj[nodeName] = xmlToJson(item);
-      } else {
-        if (typeof(obj[nodeName].push) == "undefined") {
-          var old = obj[nodeName];
-          obj[nodeName] = [];
-          obj[nodeName].push(old);
-        }
-        obj[nodeName].push(xmlToJson(item));
-      }
-    }
-  }
-  return obj;
-};
+//   // do children
+//   if (xml.hasChildNodes()) {
+//     for(var i = 0; i < xml.childNodes.length; i++) {
+//       var item = xml.childNodes.item(i);
+//       var nodeName = item.nodeName;
+//       if (typeof(obj[nodeName]) == "undefined") {
+//         obj[nodeName] = xmlToJson(item);
+//       } else {
+//         if (typeof(obj[nodeName].push) == "undefined") {
+//           var old = obj[nodeName];
+//           obj[nodeName] = [];
+//           obj[nodeName].push(old);
+//         }
+//         obj[nodeName].push(xmlToJson(item));
+//       }
+//     }
+//   }
+//   return obj;
+// };
 
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
@@ -47,24 +47,26 @@ function xmlToJson(xml) {
  *
  * @type {string}
  */
-var VIDEO = 'R Kelly';
+var VIDEO;
 
-var QUERY = 'kittens';
+// var QUERY = 'kittens';
 
 var videoGenerator = {
 
-  searchOnYouTube: 'https://gdata.youtube.com/feeds/api/videos?' +
-      'q=' + encodeURIComponent(VIDEO) + '&' +
-      'max-results=5',
+  searchOnYouTube: function(vid){
+    return 'https://gdata.youtube.com/feeds/api/videos?' +
+      'q=' + encodeURIComponent(vid) + '&' +
+      'max-results=5'
+  },
 
-  searchOnFlickr_: 'https://secure.flickr.com/services/rest/?' +
-      'method=flickr.photos.search&' +
-      'api_key=90485e931f687a9b9c2a66bf58a3861a&' +
-      'text=' + encodeURIComponent(QUERY) + '&' +
-      'safe_search=1&' +
-      'content_type=1&' +
-      'sort=interestingness-desc&' +
-      'per_page=20',
+  // searchOnFlickr_: 'https://secure.flickr.com/services/rest/?' +
+  //     'method=flickr.photos.search&' +
+  //     'api_key=90485e931f687a9b9c2a66bf58a3861a&' +
+  //     'text=' + encodeURIComponent(QUERY) + '&' +
+  //     'safe_search=1&' +
+  //     'content_type=1&' +
+  //     'sort=interestingness-desc&' +
+  //     'per_page=20',
 
   /**
    * Sends an XHR GET request to grab photos of lots and lots of kittens. The
@@ -72,16 +74,17 @@ var videoGenerator = {
    *
    * @public
    */
-  requestKittens: function() {
-    var req = new XMLHttpRequest();
-    req.open("GET", this.searchOnFlickr_, true);
-    req.onload = this.showPhotos_.bind(this);
-    req.send(null);
-  },
+  // requestKittens: function() {
+  //   var req = new XMLHttpRequest();
+  //   req.open("GET", this.searchOnFlickr_, true);
+  //   req.onload = this.showPhotos_.bind(this);
+  //   req.send(null);
+  // },
 
-  requestVideos: function() {
+  requestVideos: function(vid) {
+    console.log('requestVideos ln83', vid);
     var req = new XMLHttpRequest();
-    req.open("GET", this.searchOnYouTube, true);
+    req.open("GET", this.searchOnYouTube(vid), true);
     req.onload = this.showSearchResults.bind(this);
     req.send(null);
     // $.ajax({
@@ -107,24 +110,22 @@ var videoGenerator = {
       var $link = $(result[i].querySelector('id')).text();
       var string = JSON.stringify($link);
       var link = 'http://youtube.com/watch?v=' + string.slice(-12,-1);
-      console.log(link);
       var $title = $(result[i].querySelector('title')).text();
-      $('body').append('<div><a href='+ link + '>' + $title + '</a><div>');
-    }
-    console.log(result);
-  },
-
-  showPhotos_: function (e) {
-    var kittens = e.target.responseXML.querySelectorAll('photo');
-      console.log(kittens);
-    for (var i = 0; i < kittens.length; i++) {
-      var img = document.createElement('img');
-      img.src = this.constructKittenURL_(kittens[i]);
-      img.setAttribute('alt', kittens[i].getAttribute('title'));
-      document.body.appendChild(img);
-
+      $('.show').append('<div><a href='+ link + '>' + $title + '</a><div>');
     }
   },
+
+  // showPhotos_: function (e) {
+  //   var kittens = e.target.responseXML.querySelectorAll('photo');
+  //     console.log(kittens);
+  //   for (var i = 0; i < kittens.length; i++) {
+  //     var img = document.createElement('img');
+  //     img.src = this.constructKittenURL_(kittens[i]);
+  //     img.setAttribute('alt', kittens[i].getAttribute('title'));
+  //     document.body.appendChild(img);
+
+  //   }
+  // },
 
   /**
    * Given a photo, construct a URL using the method outlined at
@@ -134,20 +135,29 @@ var videoGenerator = {
    * @return {string} The kitten's URL.
    * @private
    */
-  constructVideoURL: function(video){
+  // constructVideoURL: function(video){
 
-  },
+  // },
 
-  constructKittenURL_: function (photo) {
-    return "http://farm" + photo.getAttribute("farm") +
-        ".static.flickr.com/" + photo.getAttribute("server") +
-        "/" + photo.getAttribute("id") +
-        "_" + photo.getAttribute("secret") +
-        "_s.jpg";
-  }
+  // constructKittenURL_: function (photo) {
+  //   return "http://farm" + photo.getAttribute("farm") +
+  //       ".static.flickr.com/" + photo.getAttribute("server") +
+  //       "/" + photo.getAttribute("id") +
+  //       "_" + photo.getAttribute("secret") +
+  //       "_s.jpg";
+  // }
 };
 
 $(function(){
+
+  $('form').submit(function(event){
+    event.preventDefault();
+    VIDEO = $('input').val();
+    $('input').val('');
+    $('.show').html('')
+    console.log(VIDEO);
+    videoGenerator.requestVideos(VIDEO);
+  });
   // kittenGenerator.requestKittens();
-  videoGenerator.requestVideos();
+  // videoGenerator.requestVideos();
 })
